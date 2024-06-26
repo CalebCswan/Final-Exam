5f08071c5971be9678cb6abbe9a1a93d6b5770cf
CalebCswan <arachnidmale@outlook.com>
2024-06-26 16:48:30 -0500

    Add files via upload

---
+++ Question4.js
@@ -0,0 +1,21 @@
+class Node {
+    constructor(value = null, next = null, prev = null) {
+      this.value = value;
+      this.next = next;
+      this.prev = prev;
+    }
+  } 
+  
+  class CircularDoublyLinkedList {
+    constructor(head = null) {
+      this.head = head;
+    }
+
+    constructor deleteAt(index); 
+        head.next > 4 > 5 > 6 < 4;
+        head.last > 4 > 6 < 5 < 4;
+    
+
+  console.log(deleteAt);
+
+  Module.exports = { Node, CircularDoublyLinkedList };
