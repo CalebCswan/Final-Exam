99d8082fcd398c82148a74b87fc780c9223589f0
CalebCswan <arachnidmale@outlook.com>
2024-06-26 16:26:26 -0500

    Add files via upload

---
+++ Question3.js
@@ -0,0 +1,22 @@
+function navigateMaze(maze, instructions) {
+    maze = 
+    [[0, 1, 4, 3], 
+    [6, 2, 5, 7], 
+    [5, 1, 7, 4], 
+    [0, 2, 3, 8]]
+
+instructions = ["up", "right", "left", "down", "up", "left"]
+    var queue = [];
+queue.push(2);        
+queue.push(5);         
+var i = queue.shift(); 
+alert(i);    
+
+return
+false
+}
+
+console.log(navigateMaze);
+
+
+  
