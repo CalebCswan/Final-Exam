e6741dd27330243a8aa9843bc59c79ccee06bc99
CalebCswan <arachnidmale@outlook.com>
2024-07-10 10:05:14 -0500

    Add files via upload

---
+++ Question8.go
@@ -0,0 +1,24 @@
+package main
+
+import ("fmt")
+
+func() Result[T any] struct {
+	DuplicatedElements []T
+	NumberOfDuplicates uint
+	}
+
+func FindDuplicates [T any](elements []T) Result[T] {
+	elements = [a, b, c, d, a, c]
+	return DuplicatedElements: [a, c]
+	return NumberOfDuplicates: 2
+	fmt.Println(sl)
+	}
+
+	func findElements {[7, 7, 3, 3, 5, 2, 2, 5]
+	return DuplicatedElements: [2, 3, 5, 7]
+	NumberOfDuplicates: 4
+	fmt.Println(sl)
+	}
+	
+}
+
