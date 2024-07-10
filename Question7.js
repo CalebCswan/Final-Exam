f146d2ca449e65c431a8020c5a2bdf3d83a6cbd2
CalebCswan <arachnidmale@outlook.com>
2024-07-10 10:04:52 -0500

    Add files via upload

---
+++ Question7.js
@@ -0,0 +1,20 @@
+function harderFizzBuzz(n) {
+
+    Let n = 30
+
+    for range nums(1->n) {
+        if value/3 = num, 
+        return "Fizz";
+
+        if value/=5 num,
+        return "Buzz";
+
+        if value/=3 num || value/=5 num,
+        return "FizzBuzz";
+    }
+
+}   
+
+console.log(harderFizzBuzz);
+
+  module.exports = harderFizzBuzz;
