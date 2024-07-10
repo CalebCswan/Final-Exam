6a93d609d6223509d888f0f9e8ec8cfe832f2310
CalebCswan <arachnidmale@outlook.com>
2024-07-10 10:02:06 -0500

    Add files via upload

---
+++ Question5.go
@@ -0,0 +1,24 @@
+package code
+
+struct binary_tree_node
+{
+  binary_tree_node * left_child;
+  binary_tree_node * right_child;
+  value = 7
+};
+type BST struct {
+	Value int
+	Left  *BST
+	Right *BST
+}
+
+func InsertIntoBST(root *BST, value int) *BST {
+	if(root == NULL)
+        return getNewNode(val);
+	else if(root < right_child)
+		return getNewNode(val);
+	else if(root > right_child)
+	return root
+}
+
+fmt.Println(InsertIntoBST)
